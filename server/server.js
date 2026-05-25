3440d0cb1d5c35173b5c8400f83a7b9efee676f1
AhmedEzzatTawfiq <Ahmedezzat672001@gmail.com>
2026-05-26 00:04:44 +0200

    edit-on-server

--- server/server.js
+++ server/server.js
@@ -41,8 +41,10 @@ app.get("/", (req, res) => {
 })
 
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
+if (!process.env.VERCEL) {
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`)
+    })
+}
 
 export default app;
