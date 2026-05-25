5351b33e01ec969c553999b4f1d3132a1eebfa41
AhmedEzzatTawfiq <Ahmedezzat672001@gmail.com>
2026-05-25 18:27:13 +0200

    server

--- server/server.js
+++ server/server.js
@@ -7,37 +7,42 @@ import adminRouter from "./routes/adminRoute.js";
 import doctorRouter from "./routes/doctorRoute.js";
 import userRouter from "./routes/userRoute.js";
 
-
-
-
 dotenv.config({ path: ['.env.local', '.env'] });
-//app config
+
 const app = express();
 const port = process.env.PORT || 5000;
+
+// Connect database & cloudinary
 connectDB();
 connectCloudinary();
 
-//middleware
+// Middleware
 app.use(express.json());
-app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:5174', 'https://find-your-doctor-ashen.vercel.app/'],
-    credentials: true
-}))
 
-//api endpoints
+app.use(cors({
+    origin: [
+        "http://localhost:3000",
+        "http://localhost:5173",
+        "https://find-your-doctor-ashen.vercel.app"
+    ],
+    credentials: true
+}));
+
+// Routes
 app.use("/api/admin", adminRouter);
 app.use("/api/doctor", doctorRouter);
 app.use("/api/user", userRouter);
 
-
-
 app.get("/", (req, res) => {
-    res.send("Api working")
-})
+    res.send("API Working");
+});
 
+// Run locally only
+if (process.env.VERCEL !== "1") {
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+}
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
-
+// Export for Vercel
 export default app;
